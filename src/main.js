ce5750555287a3a661babcfd364084e5c082193d
yangyong520 <1270201010@qq.com>
2017-11-29 12:06:50 +0800

    合并main.js

--- src/main.js
+++ src/main.js
@@ -17,6 +17,23 @@ Object.defineProperty(Vue.prototype, '$axios', { value: axiosConfig });
 import util from './util.js'
 Vue.use(util)
 
+
+// 引入vue-amap地图组件
+import AMap from 'vue-amap';
+Vue.use(AMap);
+
+// 初始化vue-amap
+AMap.initAMapApiLoader({
+  // 申请的高德key
+  key: '300587de5456a07509bd511d9b1d0475',
+  // 插件集合
+  plugin: ['']
+});
+
+
+
+
+
 /*引入全局组件*/
 import KdowInput from './components/KdowInput.vue'
 import KdowForm from './components/KdowForm.vue'
@@ -26,6 +43,12 @@ import KdowMenu from './components/KdowMenu.vue'
 import KdowFilpPages from './components/KdowFilpPages.vue'
 import KdowHeader from './components/KdowHeader.vue'
 import KdowLoginDialog from './components/KdowLoginDialog.vue'
+import KdowFooter from './components/KdowFooter.vue'
+import KdowDelet from './components/KdowDelet.vue'
+import KdowProfile from './components/KdowProfile.vue'
+import kdowUserHeader from './components/KdowUserHeader.vue'
+import kdowUpphoto from './components/KdowUpphoto.vue'
+
 
 /*注册全局组件*/
 Vue.component("kdow-input",KdowInput)
@@ -36,6 +59,11 @@ Vue.component("kdow-menu", KdowMenu)
 Vue.component("kdow-filp-pages", KdowFilpPages)
 Vue.component("kdow-header", KdowHeader)
 Vue.component("kdow-login-dialog", KdowLoginDialog)
+Vue.component("kdow-footer",KdowFooter)
+Vue.component("kdow-delet",KdowDelet)
+Vue.component("kdow-profile",KdowProfile)
+Vue.component("kdow-userheader",kdowUserHeader)
+Vue.component("kdow-upphoto",kdowUpphoto)
 
 /*引入全局插件*/
 import Toast from './myPlugins/Toast/Toast.js'
