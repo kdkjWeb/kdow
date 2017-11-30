d20194e3c0158179d63399f5e5ea8f67eac3d559
sherllyLXL <511987975@qq.com>
2017-11-30 18:42:36 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -29,4 +29,4 @@ export default {
   .loading{position: absolute;z-index: 9999; top:100px; left:200px;}
   .loading_shadow{position:fixed;width:100%;height:1800px;left:0; top:0; opacity: 0.5;z-index: 9999;}
   .loading_img{position: fixed; z-index: 9999; left: 40%; top:30%; width:40px;height:40px;padding:60px 100px;box-sizing: content-box; background-color: #333;border-radius: 10px;   filter:alpha(opacity=50);  -moz-opacity:0.5;  -khtml-opacity: 0.5; opacity: 0.5;  }
-</style>
+</style>   
